f68e007095b2d86f9d9333cedd405361d06f5c18
weynard02 <samsicoweynard@gmail.com>
2023-09-30 17:31:20 +0700

    feat: create Song model + controller

---
+++ app/Models/Song.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    use HasFactory;
+
+    protected $table = 'songs';
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['title', 'artist', 'release_date','file_audio_path'];
+}
