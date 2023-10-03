ade5ca85e08faf6e5f21880909da53428611dbcb
Akhmad Mustofa Solikin <91455464+mustofa08@users.noreply.github.com>
2023-10-03 15:56:49 +0700

    Update Artist.php

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Artist extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function song(): HasMany
+    {
+        return $this->hasMany(Song: :class);
+    }
 }
