4c292d1ee5347dd4304800ec0ca4008f57a5f605
weynard02 <samsicoweynard@gmail.com>
2023-10-03 15:14:22 +0700

    feat: make song seeder

---
+++ database/seeders/SongSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Artist;
+use App\Models\Song;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SongSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Song::factory()->count(10)->for(Artist::factory()->state([
+            'name' => 'Raon Invers'
+        ]))->create();
+    }
+}
