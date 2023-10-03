15f2454799e62ab5c663b46f6190018820e3776f
weynard02 <samsicoweynard@gmail.com>
2023-10-03 14:53:35 +0700

    feat: create relation from song

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Song extends Model
 {
@@ -13,4 +14,9 @@ class Song extends Model
     protected $primaryKey = 'id';
 
     protected $fillable = ['title', 'release_date','file_audio_path', 'thumbnail_path', 'tags'];
+
+    public function artist(): BelongsTo 
+    {
+        return $this->belongsTo(Artist::class);
+    }
 }
