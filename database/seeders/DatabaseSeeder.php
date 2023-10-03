a99caeee64cfff221c870bd6d68b39febb54889d
Akhmad Mustofa Solikin <91455464+mustofa08@users.noreply.github.com>
2023-10-03 15:33:46 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,31 @@ class DatabaseSeeder extends Seeder
             SongSeeder::class,
         ]); 
 
+        
     }
+    
+    User: :create([
+        'name' => 'Akhmad Mustofa Solikin',
+        'Nrp' => '502521130',
+    ])
+
+    User: :create([
+        'name' => 'Alexander Weynard Samsico',
+        'Nrp' => '5025211014',
+    ])            
+            
+    Category: :create([
+        'name' => 'Music',
+        'slug' => 'musik'
+     ]);    
+
+    Category: :create([
+        'title' => 'Music',
+        'slug' => 'music'
+        'excerpt' => 'Musik adalah cabang seni yang membahas dan menetapkan berbagai suara ke dalam pola-pola yang dapat dimengerti dan dipahami manusia. Musik telah menjadi bagian dari kehidupan manusia sehari-hari. Bahkan, ada orang-orang yang menjadikan musik sebagai kebutuhan hidup. Dirinya baru bisa berfungsi dengan baik apabila beraktivitas sambil mendengarkan musik.',
+        'body' => 'Musik adalah cabang seni yang membahas dan menetapkan berbagai suara ke dalam pola-pola yang dapat dimengerti dan dipahami manusia. Musik telah menjadi bagian dari kehidupan manusia sehari-hari. Bahkan, ada orang-orang yang menjadikan musik sebagai kebutuhan hidup. Dirinya baru bisa berfungsi dengan baik apabila beraktivitas sambil mendengarkan musik.
+                    Seiring waktu, musik juga mengalami perkembangan dalam hal jenis dan genre. Anda kini mengenal jenis musik pop, rock, blues, indie, metal, dan sebagainya sebagai bagian dari jenis-jenis musik yang beredar di masyarakat. Sangat menarik membahas tentang pengertian musik dan juga unsur-unsurnya. Berikut ulasan selengkapnya, yang patut Anda baca lebih lanjut.',
+        'category_id' => 1,
+        'user_id' => 1
+         ]);   
 }
