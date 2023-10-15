b30b3d5a95a80e31ae1e6383960e46d0ec2ffa87
weynard02 <samsicoweynard@gmail.com>
2023-10-15 14:58:48 +0700

    fix: route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/form', [SongController::class, 'create']);
+Route::post('/submit', [SongController::class, 'store']);
 
 require __DIR__.'/auth.php';
