e3fd22b8f51c0446f05ab03c8fe927091d486439
weynard02 <samsicoweynard@gmail.com>
2023-10-16 10:51:01 +0700

    fix: text in dark mode

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
             @endif
             <!-- Jumbotron -->
         <div class="py-24 px-6 text-center">
-            <h1 class="mt-2 mb-16 text-5xl text-slate-50 font-bold tracking-tight md:text-6xl xl:text-7xl">
+            <h1 class="mt-2 mb-16 text-5xl dark:text-slate-50 font-bold tracking-tight md:text-6xl xl:text-7xl">
               The best Music Streaming Database <br /><span class="text-sky">for your entertainment</span>
             </h1>
             <a class="mb-2 inline-block rounded bg-blue-500 px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-blue-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-blue-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] md:mr-2 md:mb-0"
