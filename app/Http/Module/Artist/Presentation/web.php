80f5aa67418da8f4cbf7b12712ad289b1baf43d2
weynard02 <samsicoweynard@gmail.com>
2023-10-17 15:24:00 +0700

    fix:route

--- app/Http/Module/Artist/Presentation/web.php
+++ app/Http/Module/Artist/Presentation/web.php
@@ -1,3 +1,8 @@
 <?php
+use Illuminate\Support\Facades\Route;
+
+Route::get('ping', function(){
+    return csrf_token();
+});
 
 Route::post('create_artist', [\App\Http\Module\Artist\Presentation\Controller\ArtistController::class, 'createArtist']);
