caf5c3df80dfd3ece6a1eebc3984965523eae154
weynard02 <samsicoweynard@gmail.com>
2023-10-31 20:20:05 +0700

    feat: add thumbnail show

--- resources/views/song.blade.php
+++ resources/views/song.blade.php
@@ -9,7 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg ">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <audio controls style="width: 100%; max-width: 600px">
+                    @if ($song->thumbnail_path)
+                        <img src="{{ asset('storage/thumbnails/' . $song->thumbnail_path) }}" class="h-auto max-w-lg rounded-t-lg" alt="Thumbnail not uploaded">
+                    @else
+                        <img src="{{ asset('default_thumbnail.jpeg') }}" class="h-auto max-w-lg rounded-t-lg">
+                    @endif
+                    <audio controls style="width: 100%;">
                         <source src="{{ asset('storage/songs/'. $song->file_audio_path)}}" type="audio/mpeg">
                     Your browser does not support the audio element.
                     </audio>
