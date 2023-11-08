b4f231a9cd1fc6778795695e697057d1fcdba26f
weynard02 <samsicoweynard@gmail.com>
2023-11-08 13:35:17 +0700

    add Genre/Song Seeder

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Genre;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class GenreSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $genres = array("pop", "jazz", "hiphop", "rock", "classic", "acoustic", "live", "kpop", "jpop", "indie", "sleep", "party", "love", "metal", "anime", "podcast", "gaming");
+        $len = count($genres);
+        for($i=0;$i<$len;$i++) {
+            Genre::create([
+                'name' => $genres[$i],
+            ]);
+        }
     }
 }
