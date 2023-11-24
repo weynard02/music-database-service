6ebe6fcb3c42aebdbfd290c819a45fb5ba510282
weynard02 <samsicoweynard@gmail.com>
2023-11-24 14:06:21 +0700

    fix: genre capitalize

--- resources/views/addgenre.blade.php
+++ resources/views/addgenre.blade.php
@@ -13,7 +13,7 @@
                     @foreach($genres as $genre)
                         <div class="flex items-center mb-4">
                             <input {{ $genre->songs->contains($song) ? 'checked' : '' }} id="default-checkbox" type="checkbox" name="genres[]" value="{{$genre->id}}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{$genre->name}}</label>
+                            <label for="default-checkbox" class="capitalize ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{$genre->name}}</label>
                         </div>
                     @endforeach
                 </select>
