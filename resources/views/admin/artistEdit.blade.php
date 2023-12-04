18d9c69fdb661ac02a5b6996971cc583f1abdfc7
weynard02 <samsicoweynard@gmail.com>
2023-12-04 22:46:01 +0700

    fix: artist validation

--- resources/views/admin/artistEdit.blade.php
+++ resources/views/admin/artistEdit.blade.php
@@ -12,7 +12,7 @@
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Description <sup>*</sup></label>
-            <textarea class="form-control" id="description" rows="3" name="description" placeholder="Max 255 charaters">{{ $artist->description }}</textarea>
+            <textarea class="form-control" id="description" rows="3" name="description" placeholder="Max 1024 charaters">{{ $artist->description }}</textarea>
         </div>
         @error('name')
             <div class="alert alert-danger">{{ $message }}</div>
